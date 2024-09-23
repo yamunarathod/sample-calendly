969c3733fc2852c2cd147d84ec12b35e906f3c8a
devilseye-from666 <dhritimannarh1008@gmail.com>
2024-09-23 13:44:58 +0530

    second commit

--- api/server.js
+++ api/server.js
@@ -5,13 +5,20 @@ const app = express();
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// Middleware for serving static files (CSS, JS)
+// Adjust paths to static files and views
 app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../views'));
 
 // Route for the homepage
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname, '../views/index'));
+  res.render('index'); // Render the EJS view 'index.ejs'
 });
 
-// Export the app as a serverless function
+// Start the server and log a message
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Export the app for serverless deployment (used by Vercel)
 module.exports = app;
